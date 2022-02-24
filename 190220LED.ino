d8d467a5611953a181670195cf4c9eba81355340
troll-eunwoo <100338215+troll-eunwoo@users.noreply.github.com>
2022-02-24 15:42:24 +0900

    Add files via upload

---
+++ 190220LED.ino
@@ -0,0 +1,15 @@
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  }
+  
+void loop() {
+  digitalWrite(2, HIGH);
+  delay(1);
+  digitalWrite(2,LOW);
+  delay(1);     
+ 
+
+
+}
